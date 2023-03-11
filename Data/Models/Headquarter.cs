4ee7f62457a3a7a47aeeb154d96b76938c22b292
Jean ROBERT <jean_robert92@live.fr>
2023-03-11 12:18:42 +0100

    Installation des paquets nécessaires au projet et réalisation des modèles.

--- Data/Models/Headquarter.cs
+++ Data/Models/Headquarter.cs
@@ -2,5 +2,7 @@
 {
     public class Headquarter
     {
+        public int Id { get; set; }
+        public string City { get; set; }
     }
 }
--- Data/Models/Salary.cs
+++ Data/Models/Salary.cs
@@ -1,6 +1,19 @@
-﻿namespace DotAgroAPI.Data.Models
+﻿using System.Runtime.CompilerServices;
+
+namespace DotAgroAPI.Data.Models
 {
     public class Salary
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Gender { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string Image { get; set; }
+        public int IdService { get; set; }
+        public int IdHeadquarter { get; set; }
+        public virtual Service IdServiceNavigation { get; set; }
+        public virtual Headquarter IdHeadquarterNavigation { get; set; }
     }
 }
